b3d05d0762e7ad0edcda2013f7b80974f904f648
natimoliki <136642378+natimoliki@users.noreply.github.com>
2023-07-23 22:43:53 +0400

    28

---
+++ style/script.js
@@ -0,0 +1,8 @@
+let navBar = document.getElementById("navBar");
+let burger = document.getElementById("burgerBar");
+
+burger.addEventListener("click", function () {
+  navBar.classList.toggle("activeNav");
+});
+
+
